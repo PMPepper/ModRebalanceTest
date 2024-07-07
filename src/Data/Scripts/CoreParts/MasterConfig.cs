3802b838dd09f9299563c7ec73ea973d34005334
DESKTOP-2D91HMA\Paul <pm_pepper@hotmail.com>
2024-07-07 16:29:53 +0100

    Added 300mm gun

--- src/Data/Scripts/CoreParts/MasterConfig.cs
+++ src/Data/Scripts/CoreParts/MasterConfig.cs
@@ -10,7 +10,7 @@ namespace Scripts
             // Enable your config files using the follow syntax, don't include the ".cs" extension:
             // ConfigFiles(Your1stConfigFile, Your2ndConfigFile, Your3rdConfigFile);
 
-            PartDefinitions(RvBC100mmTurret, RvBC200mmTurret);
+            PartDefinitions(RvBC100mmTurret, RvBC200mmTurret, RvBC300mmTurret);
         }
     }
 }
